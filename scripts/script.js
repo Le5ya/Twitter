c63ca4602d1ce7b4767346df008c2554d09844a4
Le5ya <49882766+Le5ya@users.noreply.github.com>
2021-01-21 17:27:36 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,93 @@
+class FetchData {
+  getResourse = async url => {
+    const res = await fetch(url);
+      if(!res.ok) {
+        throw new Error("Error" + res.status);
+      }
+    return res.json();
+  }
+  getPost = async() => await this.getResourse('db/dataBase.json'); 
+  
+}
+const obj = new FetchData();
+
+obj.getPost().then((data) => {
+  console.log(data);
+});
 class Twitter {
   constructor({ listElem }) {
-    this.tweets = new Posts({});
+    const fetchData = new FetchData();
+    this.tweets = new Posts();
     this.elements = {
       listElem: document.querySelector(listElem),
     }
+    fetchData.getPost()
+      .then(data => {
+        data.forEach(this.tweets.addPost)
+        this.showAllPost();
+        });
+    
+  
   }
-  renderPosts() {
+  renderPosts(tweets) {
+    this.elements.listElem.textContent = '';
+  tweets.forEach(({ id, userName, nickname, postDate, text, img, likes }) => {
 
+    this.elements.listElem.insertAdjacentHTML('beforeend', `
+    <li>
+      <article class="tweet">
+        <div class="row">
+          <img class="avatar" src="images/${nickname}.jpg" alt="Аватар пользователя ${nickname}">
+          <div class="tweet__wrapper">
+            <header class="tweet__header">
+              <h3 class="tweet-author">${userName}
+                <span class="tweet-author__add tweet-author__nickname">@${nickname}</span>
+                <time class="tweet-author__add tweet__date">${postDate}</time>
+              </h3>
+              <button class="tweet__delete-button chest-icon data-id="${id}"></button>
+            </header>
+            <div class="tweet-post">
+              <p class="tweet-post__text">${text}</p>
+              ${img ?
+              `<figure class="tweet-post__image">
+                <img src="${img}" alt="${text}">
+              </figure>` :
+              ''
+            }
+            </div>
+          </div>
+        </div>
+        <footer>
+          <button class="tweet__like">
+            ${likes}
+          </button>
+        </footer>
+      </article>
+    </li>
+       `);
+    })
   }
   showUserPost() {
-
+    
   }
 
   showLikesPost() {
 
   }
-  showAlPost() {
-
+  showAllPost() {
+    this.renderPosts(this.tweets.posts);
   }
   showOpenModal() {
 
   }
 }
 
-
 class Posts {
   constructor({ posts = [] } = {}) {
     this.posts = posts;
   }
-  addPost(tweet) {
-      this.posts.push(new Post(tweet));
+  addPost = (tweets) => {
+    this.posts.push(tweets);
   }
   deletePost(id) {
 
@@ -39,26 +97,39 @@ class Posts {
   }
 }
 class Post {
-  constructor(param) {
-    this.id = param.id;
-    this.userName = param.userName;
-    this.nickName = param.nickName;
-    this.postDate = param.post.Date;
-    this.text = param.text;
-    this.img = param.img;
-    this.likes = param.likes;
+  constructor({ id, userName, nickname, postDate, text, img, likes = 0 }) {
+    
+    this.id = id || this.generateID();
+    this.userName = userName;
+    this.nickname = nickname;
+    this.postDate = post.Date ? new Date(postDate) : new Date();
+    this.text = text;
+    this.img = img;
+    this.likes = likes;
     this.liked = false;
   }
   changeLike() {
-    this.like = !this.like;
-    if(this.like) {
+    this.liked = !this.liked;
+    if(this.liked) {
       this.likes++;
     } else {
       this.likes--;
     }
+  }
+  generateID() {
+    return Math.random().toString(32).substring(2, 9) + (+new Date).toString(32);
   } 
+ getDate(){
+    const options = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+    return this.postData.toLocaleString('ru-Ru', options);
+ }
 }
 const twitter = new Twitter({
-  listElem: '.tweet-list'
+  listElem: '.tweet-list',
 })
- console.log('twitter: ', twitter);
